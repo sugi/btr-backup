ced13ee309eee002c9ebeef97e24e2cdedb54ce6
Tatsuki Sugiura <sugi@nemui.org>
2012-02-09 12:01:29 +0900

    update crontab sample.

--- debian/btr-backup.cron.d
+++ debian/btr-backup.cron.d
@@ -1,12 +1,13 @@
 PATH=/usr/local/sbin:/usr/sbin:/sbin:/usr/local/bin:/usr/bin:/bin
+BTR_BACKUP=ionice -c 3 /usr/sbin/btr-backup
 
 # Backup
-#2 3 */2 * *	root	ionice -c 3 /usr/sbin/btr-backup /root
-#6 3 */2 * *	root	ionice -c 3 /usr/sbin/btr-backup /boot
-#10 3 */2 * *	root	ionice -c 3 /usr/sbin/btr-backup /usr/local
-#46 3 * * *	root	ionice -c 3 /usr/sbin/btr-backup /var --exclude "/cache/apt/*" --exclude "/tmp/*" --exclude "/log/*" --exclude "/spool/squid*/*"
-#19 4 * * *	root	ionice -c 3 /usr/sbin/btr-backup /etc
-#5 5 * * *	root	ionice -c 3 /usr/sbin/btr-backup /home
+#2 3 */2 * *	root	$BTR_BACKUP /root
+#6 3 */2 * *	root	$BTR_BACKUP /boot
+#10 3 */2 * *	root	$BTR_BACKUP /usr/local
+#46 3 * * *	root	$BTR_BACKUP /var --exclude "/cache/apt/*" --exclude "/tmp/*" --exclude "/log/*" --exclude "/spool/squid*/*"
+#19 4 * * *	root	$BTR_BACKUP /etc
+#5 5 * * *	root	$BTR_BACKUP /home
 
 # Backup cleanup
 #7 7 * * *	root	/usr/sbin/btr-backup-clean /backup/*/*
